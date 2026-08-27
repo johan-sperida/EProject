11dd1ec89e16af4a706787411b7fc8331960ebec
john-sperida <j.esperida49@gmail.com>
2026-08-27 15:53:19 -0600

    Initializing project

---
+++ EProject/.pio/build/esp32doit-devkit-v1/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-7.1.9-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "7.1.9-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/jonesp/Github/EProject/EProject/.pio/build/esp32doit-devkit-v1/toolchain/toolchain-esp32.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
